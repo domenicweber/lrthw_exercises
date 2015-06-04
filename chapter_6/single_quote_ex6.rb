44112da7f1a69d7f2e62331c84b442bd41340c5e
Domenic Weber <domenic.weber@gmail.com>
2015-06-04 14:49:52 -0600

    Updates ex6.rb, Readme.md and adds single_quote_ex56.rb
    [#95933768]

--- chapter_6/single_quote_ex6.rb
+++ chapter_6/single_quote_ex6.rb
@@ -1,7 +1,7 @@
 # sets types of people to 10
 types_of_people = 10
 # sets x so we never have to type the sentence below again
-x = "There are #{types_of_people} types of people."
+x = 'There are #{types_of_people} types of people.'
 # lets add some redundancy for fun
 binary = 'binary'
 # and even better we can undo contractions making
@@ -9,7 +9,7 @@ binary = 'binary'
 do_not = "don't"
 # because we added more work last line lets do less by never
 # writing the 'y' again
-y = "Those who know #{binary} and those who #{do_not}"
+y = 'Those who know #{binary} and those who #{do_not}'
 
 # will print the x sentence
 puts x
@@ -17,14 +17,14 @@ puts x
 puts y
 
 # now we never have to write sentence X again
-puts "I said: #{x}."
+puts 'I said: #{x}.'
 # and the same goes for sentence Y
-puts "I also said: '#{y}'."
+puts 'I also said: '#{y}'.'
 
 # every time the variable 'hilarious' is used it will says "False"
 hilarious = false
 # another sentence contraction which uses "False"
-joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+joke_evaluation = "'Isn't that joke so funny?! #{hilarious}'"
 
 puts joke_evaluation
 
