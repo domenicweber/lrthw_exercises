d7506035b0c835e5a960bf74bfc5caf905eb3ed5
Domenic Weber <domenic.weber@gmail.com>
2015-06-06 15:48:56 -0600

    Updates readme.md and ex8.rb to complete
    [#95933776]

--- chapter_8/Readme.md
+++ chapter_8/Readme.md
@@ -1,7 +1,11 @@
-http://ruby.learncodethehardway.org/book/ex8.html
+> In Chapter 6, you did string interpolation.  In this chapter you did string formatting.
 
-In Chapter 6, you did string interpolation.  In this chapter you did string formatting.
+> Q) Do you like string interpolation or format strings?  Why?
 
-Q) Do you like string interpolation or format strings?  Why?
+> Add the answer to this question into Readme.md.
 
-Add the answer to this question into Readme.md.
+Interpolation appears to be a lot less typing.  It seems like you have to type everything twice in chapter 8.
+
+If you just used different variables for lines 3, 4 and 5 you wouldn't have to redifine what first, second, third, fourth meant each time.
+
+However, writing out line 6 would be a lot more work if you didn't define the variable 'formatter' at the begginning.
--- chapter_8/ex8.rb
+++ chapter_8/ex8.rb
@@ -3,7 +3,9 @@ formatter = '%{first} %{second} %{third} %{fourth}'
 puts formatter % { first: 1, second: 2, third: 3, fourth: 4 }
 puts formatter % { first: 'one', second: 'two', third: 'three', fourth: 'four' }
 puts formatter % { first: true, second: false, third: true, fourth: false }
-puts formatter % { first: formatter, second: formatter, third: formatter, fourth: formatter }
+puts formatter % { first: formatter, second:
+   # breaking up line for rubotyrant
+   formatter, third: formatter, fourth: formatter }
 
 puts formatter % {
   first: 'I had this thing.',
@@ -12,16 +14,12 @@ puts formatter % {
   fourth: 'So I said goodnight.'
 }
 
-
-foo = {
-  first: 'I had this thing.',
-  second: 'That you could type up right.',
-  third: "But it didn't sing.",
-}
-puts formatter % foo
-
-puts formatter % {
-
-  }
-
-puts 'more junk'
+# This is how we can fix the % auto indent bug in RM
+# foo = {
+# first: 'I had this thing.',
+# second: 'That you could type up right.',
+# third: "But it didn't sing.",
+# }
+# puts formatter % foo
+# puts formatter % {
+# }
