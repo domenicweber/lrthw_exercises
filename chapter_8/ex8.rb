0bc3b39972f8ca63fe93faa2d6416c3a2bb5a97e
Domenic Weber <domenic.weber@gmail.com>
2015-06-05 22:15:04 -0600

    Updates ex8.rb but not finished
    [#95933776]

--- chapter_8/ex8.rb
+++ chapter_8/ex8.rb
@@ -6,11 +6,11 @@ puts formatter % { first: true, second: false, third: true, fourth: false }
 puts formatter % { first: formatter, second: formatter, third: formatter, fourth: formatter }
 
 puts formatter % {
-    first: 'I had this thing.',
-    second: 'That you could type up right.',
-    third: "But it didn't sing.",
-    fourth: 'So I said goodnight.'
-  }
+  first: 'I had this thing.',
+  second: 'That you could type up right.',
+  third: "But it didn't sing.",
+  fourth: 'So I said goodnight.'
+}
 
 
 foo = {
